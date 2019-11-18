ff0d21418ca037f45750bcf75c8bb89cac1ff54c
priyadharshini13 <dharshini.dp95@gmail.com>
2019-11-18 18:23:31 +0000

    First Commit - UI package

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    import { Route } from 'react-router-dom';
+    import React, { Component } from 'react';
+    import Customer from './Customer';
+    import Support from './Support';
 
-export default App;
+    import './normalize.css';
+    import './skeleton.css';
+    import './App.css';
+    class App extends Component {
+      render() {
+        return (
+          <div className="App">
+            <Route exact path="/" render={() => <Customer />} />
+            <Route exact path="/support" render={() => <Support />} />
+          </div>
+        );
+      }
+    }
+
+    export default App;
